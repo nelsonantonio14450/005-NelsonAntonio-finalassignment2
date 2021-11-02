62cba1ff375687c041cc6ddb9d2ab3761c2a8c63
Nelson Antonio <nelson.antonio14450@gmail.com>
2021-11-02 12:08:13 +0700

    init

---
+++ server.js
@@ -0,0 +1,27 @@
+function requireHTTPS(req, res, next) {
+    //membuat semua request yang sebelumnya http biasa menjadi https
+    if (
+        !req.secure && req.get('x-forward-proto') != 'https'
+    ) {
+        return res.redirect('https://' + req.get('host') + req.url)
+    }
+
+    next();
+}
+
+//aplikasi express
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080
+
+app
+    .use(requireHTTPS)
+    .use(express.static('./dist/payment'))
+
+
+    .get('/*', (req, res) => res.sendFile('index.html', { root: './dist/payment' }))
+
+    .listen(port, () => {
+        console.log(`my angular apps runnin hhtp://localhost:${port}`)
+    })
