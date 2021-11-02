b28c8e88ed61d7afe09cbf275bdf2f1d1c84e1d9
Nelson Antonio <nelson.antonio14450@gmail.com>
2021-11-02 13:08:15 +0700

    test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 function requireHTTPS(req, res, next) {
     //membuat semua request yang sebelumnya http biasa menjadi https
     if (
-        !req.secure && req.get('x-forward-proto') != 'https'
+        !req.secure && req.get('x-forwarded-proto') !== 'https'
     ) {
         return res.redirect('https://' + req.get('host') + req.url)
     }
