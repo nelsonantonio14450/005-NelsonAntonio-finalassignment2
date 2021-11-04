f5ffadeb63eff7444dcf185a8358e910d87de925
Nelson Antonio <nelson.antonio14450@gmail.com>
2021-11-04 11:11:52 +0700

    validators regex

--- src/app/insert-payment/insert-payment.component.ts
+++ src/app/insert-payment/insert-payment.component.ts
@@ -10,10 +10,10 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 export class InsertPaymentComponent implements OnInit {
 
   CardOwnerName = new FormControl('', [Validators.required]);
-  CardNumber = new FormControl('', [Validators.required, Validators.maxLength(16)]);
+  CardNumber = new FormControl('', [Validators.required, Validators.maxLength(16), Validators.pattern('[0-9]*')]);
   ExpirationDate = new FormControl('', [Validators.required]);
   PaidAt = new FormControl('', [Validators.required]);
-  SecurityCode = new FormControl('', [Validators.required, Validators.maxLength(3)]);
+  SecurityCode = new FormControl('', [Validators.required, Validators.maxLength(3), Validators.pattern('[0-9]*')]);
   Amount = new FormControl('', [Validators.required]);
 
   getErrorMessageCON() {
@@ -22,7 +22,8 @@ export class InsertPaymentComponent implements OnInit {
 
   getErrorMessageCN() {
     return this.CardNumber.hasError('required') ? 'you must enter a value' :
-      this.CardNumber.hasError('maxlength') ? 'max 16' : ''
+      this.CardNumber.hasError('maxlength') ? 'max 16' :
+        this.CardNumber.hasError('pattern') ? 'only number is acceptable' : ''
   }
 
   getErrorMessageED() {
@@ -35,7 +36,8 @@ export class InsertPaymentComponent implements OnInit {
 
   getErrorMessageSC() {
     return this.SecurityCode.hasError('required') ? 'you must enter a value' :
-      this.SecurityCode.hasError('maxlength') ? 'max 3' : ''
+      this.SecurityCode.hasError('maxlength') ? 'max 3' :
+        this.SecurityCode.hasError('pattern') ? 'only number is acceptable' : ''
   }
 
   getErrorMessageAmount() {
--- src/app/update-payment/update-payment.component.ts
+++ src/app/update-payment/update-payment.component.ts
@@ -9,10 +9,10 @@ import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dial
 })
 export class UpdatePaymentComponent implements OnInit {
   CardOwnerName = new FormControl('', [Validators.required]);
-  CardNumber = new FormControl('', [Validators.required, Validators.maxLength(16)]);
+  CardNumber = new FormControl('', [Validators.required, Validators.maxLength(16), Validators.pattern('[0-9]*')]);
   ExpirationDate = new FormControl('', [Validators.required]);
   PaidAt = new FormControl('', [Validators.required]);
-  SecurityCode = new FormControl('', [Validators.required, Validators.maxLength(3)]);
+  SecurityCode = new FormControl('', [Validators.required, Validators.maxLength(3), Validators.pattern('[0-9]*')]);
   Amount = new FormControl('', [Validators.required]);
 
   getErrorMessageCON() {
@@ -21,7 +21,8 @@ export class UpdatePaymentComponent implements OnInit {
 
   getErrorMessageCN() {
     return this.CardNumber.hasError('required') ? 'you must enter a value' :
-      this.CardNumber.hasError('maxlength') ? 'max 16' : ''
+      this.CardNumber.hasError('maxlength') ? 'max 16' :
+        this.CardNumber.hasError('pattern') ? 'only number is acceptable' : ''
   }
 
   getErrorMessageED() {
@@ -34,7 +35,8 @@ export class UpdatePaymentComponent implements OnInit {
 
   getErrorMessageSC() {
     return this.SecurityCode.hasError('required') ? 'you must enter a value' :
-      this.SecurityCode.hasError('maxlength') ? 'max 3' : ''
+      this.SecurityCode.hasError('maxlength') ? 'max 3' :
+        this.SecurityCode.hasError('pattern') ? 'only number is acceptable' : ''
   }
 
   getErrorMessageAmount() {
